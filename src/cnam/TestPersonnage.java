83213f88fbc7f5f8d3146afc7b418de308bb77f6
StraetenMax <maxstraeten@yahoo.com>
2017-05-18 15:29:42 +0200

    correction de la class Main

--- src/cnam/TestPersonnage.java
+++ src/cnam/TestPersonnage.java
@@ -10,6 +10,32 @@ public class TestPersonnage {
 		PersonnageJoueur per2 = new PersonnageJoueur("homme",20, 150, "Pierre");
 		System.out.println(per2.description());
 		
+        //Intancier les classes
+		Personnage P1 = new Personnage("Humain", 180, 75);
+		PersonnageJoueur PJ1 = new PersonnageJoueur("Nain", 100, 90, 30, 100);
+		PersonnageJoueurGuerrier PJG1 = new PersonnageJoueurGuerrier("Orc", 200,180,"", 40, 100);
+		PersonnageJoueurGuerrier PJG2 = new PersonnageJoueurGuerrier("Elfe", 190, 90,""; 25, 100);
+
+		PersonnageJoueurMedecin PJM1 = new vec une race "Humain", 175, 65, 20);
+		PersonnageJoueurMedecin PJM2 = new une race "Elfe"", une taille de 185, un poids de 80, un nombre de médicaments de 26
+		
+		
+		
+		
+        //appeler les méthodes
+		System.out.println(P1.description());
+		System.out.println(PJ1.description());
+		System.out.println(PJG1.description());
+		
+		System.out.println(PJG1.estMoinsFortQue(PJG2));
+		System.out.println(PJM1.plusGrandQue(PJM2));
+		 
+
+	
+	
+	
+	
+	
 	}
 
 }
