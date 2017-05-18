601754b7dfb83dd4ae4cb649886678a57fa58af0
StraetenMax <maxstraeten@yahoo.com>
2017-05-18 15:44:33 +0200

    finalisation

--- src/cnam/TestPersonnage.java
+++ src/cnam/TestPersonnage.java
@@ -12,12 +12,12 @@ public class TestPersonnage {
 		
         //Intancier les classes
 		Personnage P1 = new Personnage("Humain", 180, 75);
-		PersonnageJoueur PJ1 = new PersonnageJoueur("Nain", 100, 90, 30, 100);
-		PersonnageJoueurGuerrier PJG1 = new PersonnageJoueurGuerrier("Orc", 200,180,"", 40, 100);
-		PersonnageJoueurGuerrier PJG2 = new PersonnageJoueurGuerrier("Elfe", 190, 90,""; 25, 100);
+		PersonnageJoueur PJ1 = new PersonnageJoueur("Nain", 100, 90,"X");
+		PersonnageJoueurGuerrier PJG1 = new PersonnageJoueurGuerrier("Orc", 200,180,"X", 40, 100,true);
+		PersonnageJoueurGuerrier PJG2 = new PersonnageJoueurGuerrier("Elfe", 190, 90,"X", 25, 100,true);
 
-		PersonnageJoueurMedecin PJM1 = new vec une race "Humain", 175, 65, 20);
-		PersonnageJoueurMedecin PJM2 = new une race "Elfe"", une taille de 185, un poids de 80, un nombre de médicaments de 26
+		PersonnageJoueurMedecin PJM1 = new PersonnageJoueurMedecin("Humain", 175, 65,"X", 20);
+		PersonnageJoueurMedecin PJM2 = new PersonnageJoueurMedecin("Elfe", 185, 80, "X", 26);
 		
 		
 		
@@ -27,8 +27,13 @@ public class TestPersonnage {
 		System.out.println(PJ1.description());
 		System.out.println(PJG1.description());
 		
+		System.out.println(PJG2.description());
+		System.out.println(PJM1.description());
+		System.out.println(PJM2.description());
+		
+		
 		System.out.println(PJG1.estMoinsFortQue(PJG2));
-		System.out.println(PJM1.plusGrandQue(PJM2));
+		System.out.println(PJM1.estPlusGrandQue(PJM2));
 		 
 
 	
